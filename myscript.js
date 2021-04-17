42885986a89cd9e795fd5ab56ac54be4f7811e7b
PoloLikesCoffee <phglorianne@gmail.com>
2021-04-17 18:30:52 +0200

    first version of the function operate

--- myscript.js
+++ myscript.js
@@ -1,3 +1,53 @@
+const btns = document.querySelectorAll('button');
+const display = document.getElementById('output');
+let operator = '';
+let currentValue = 0;
+let previousValue = 0;
+
+btns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        if (event.target.hasAttribute('data-number')) {
+            //console.log('number', event.target.value);
+            
+            if(operator){
+                currentValue+=event.target.value;
+                updateDisplay(event.target.value);
+            } else {
+                updateDisplay(event.target.value);
+                previousValue+=event.target.value;
+            }
+            
+        } else if (event.target.hasAttribute('data-operator')) {
+            //console.log('operator', event.target.value);
+            if (event.target.value==='=') {
+                operate(operator, previousValue, currentValue);
+                display.textContent = '';
+                updateDisplay(operate(operator, previousValue, currentValue));
+            } else {
+                updateDisplay(event.target.value);
+                operator = event.target.value;
+            }
+        }
+        if (event.target.hasAttribute('data-clear')) {
+            //console.log('clear');
+            display.textContent = '';
+        }
+        if (event.target.hasAttribute('data-delete')) {
+            //console.log('delete');
+            let a = display.textContent;
+            let b = a.slice(0, a.length-1);
+            display.textContent = b;
+        }
+    });
+});
+
+
+function updateDisplay (number) {
+    display.textContent += number;
+    //currentValue += display.textContent;
+}
+
+
 function add (a, b) {
     return a + b;
 }
@@ -15,6 +65,18 @@ function divide (a, b) {
 }
 
 function operate (operator,a, b) {
+    switch (operator) {
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
+    //previous way
+    /*
     if (operator == '+') {
         return add(a,b);
     } 
@@ -26,5 +88,5 @@ function operate (operator,a, b) {
     }
     if (operator == '/') {
         return divide(a,b);
-    } 
+    } */
 }
