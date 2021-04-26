e8fdac81b0de0f74f391fdc1739c69efd50acf84
PoloLikesCoffee <phglorianne@gmail.com>
2021-04-26 18:44:00 +0200

    create sub functions for better lisibility

--- myscript.js
+++ myscript.js
@@ -20,111 +20,21 @@ updateDisplay();
 btns.forEach(btn => {
     btn.addEventListener('click', event => {
         if (event.target.hasAttribute('data-number')) {
-            if (firstValue === 0) {
-                // first click = input of first value
-                if (displayValue === 0) {
-                    displayValue = event.target.value;
-                } else {
-                    displayValue += event.target.value;
-                }
-            } else {
-                // third click = input of second value
-                if (displayValue === firstValue) {
-                    displayValue = event.target.value;
-                } else {
-                    displayValue += event.target.value;
-                }
-            }
+            getNumbers(event.target.value);
             updateDisplay();
         } 
         if (event.target.hasAttribute('data-operator')) { 
-            if(firstOperator != '' && secondOperator === '') {
-                //fourth click = if there is already the first operator,
-                //input of second operator
-                secondOperator = event.target.value;
-                secondValue = displayValue;
-                solution = operate(firstOperator, firstValue, secondValue);
-                if (solution === 'ERROR...ERROR') {
-                    displayValue = 'ERROR...ERROR';
-                } else {
-                    displayValue = Math.round(solution * 1000) / 1000;
-                    firstValue = displayValue;
-                    solution = 0;
-                }
-                updateDisplay();
-            } else if (firstOperator != '' && secondOperator != '') {
-                //sixth click = input of new operator
-                secondValue = displayValue;
-                solution = operate(secondOperator, firstValue, secondValue);
-                secondOperator = event.target.value;
-                if (solution === 'ERROR...ERROR') {
-                    displayValue = 'ERROR...ERROR';
-                } else {
-                    displayValue = Math.round(solution * 1000) / 1000;
-                    firstValue = displayValue;
-                    solution = 0;
-                }
-                updateDisplay();
-            } 
-            else {
-                //second click = input of first operator
-                firstOperator = event.target.value;
-                firstValue = displayValue;
-            }    
-
+            getOperators(event.target.value);
         }
         if (event.target.hasAttribute('data-equals')) {
-            if (firstOperator === '') {
-                //pressing equals when there is no operator
-                displayValue = displayValue;
-            } else if (secondOperator != '') {
-                //if there is a second operator, next equals operation
-                secondValue = displayValue;
-                solution = operate(secondOperator, firstValue, secondValue);
-                if (solution === 'ERROR...ERROR') {
-                    displayValue = 'ERROR...ERROR';
-                } else {
-                    displayValue = Math.round(solution * 1000) / 1000;
-                    firstValue = displayValue;
-                    secondValue = 0;
-                    firstOperator = '';
-                    secondOperator = '';
-                    solution = 0;
-                }
-                updateDisplay();
-            } else {
-                //first equals operaton
-                secondValue = displayValue;
-                solution = operate(firstOperator, firstValue, secondValue);
-                if (solution === 'ERROR...ERROR') {
-                    displayValue = 'ERROR...ERROR';
-                } else {
-                    displayValue = Math.round(solution * 1000) / 1000;
-                    firstValue = displayValue;
-                    secondValue = 0;
-                    firstOperator = '';
-                    secondOperator = '';
-                    solution = 0;
-                }
-                updateDisplay();
-            }
+            getEquals();
         }
         if (event.target.hasAttribute('data-clear')) {
-            //reset of all variables
-            displayValue = 0;
-            firstValue = 0;
-            secondValue = 0;
-            firstOperator = '';
-            secondOperator = '';
-            solution = 0;
+            clearEverything();
             updateDisplay();
         } 
         if (event.target.hasAttribute('data-delete')) {
-            if (displayValue === 0) {
-                displayValue = displayValue;
-            } else {
-                displayValue = displayValue.toString().slice(0, -1);
-            }
+            deleteLastChar();
             updateDisplay();
         }
     });
@@ -132,103 +42,118 @@ btns.forEach(btn => {
 
 document.addEventListener('keydown', (e) => {
     if (!isNaN(e.key)) {
-        if (firstValue === 0) {
-            // first click = input of first value
-            if (displayValue === 0) {
-                displayValue = e.key;
-            } else {
-                displayValue += e.key;
-            }
-        } else {
-            // third click = input of second value
-            if (displayValue === firstValue) {
-                displayValue = e.key;
-            } else {
-                displayValue += e.key;
-            }
-        }
+        getNumbers(e.key);
         updateDisplay();
 	}
     if (['/', '+', '-', '*'].includes(e.key)) {
-        if(firstOperator != '' && secondOperator === '') {
-            //fourth click = if there is already the first operator,
-            //input of second operator
-            secondOperator = e.key;
-            secondValue = displayValue;
-            solution = operate(firstOperator, firstValue, secondValue);
-            if (solution === 'ERROR...ERROR') {
-                displayValue = 'ERROR...ERROR';
-            } else {
-                displayValue = Math.round(solution * 1000) / 1000;
-                firstValue = displayValue;
-                solution = 0;
-            }
-            updateDisplay();
-        } else if (firstOperator != '' && secondOperator != '') {
-            //sixth click = input of new operator
-            secondValue = displayValue;
-            solution = operate(secondOperator, firstValue, secondValue);
-            secondOperator = e.key;
-            if (solution === 'ERROR...ERROR') {
-                displayValue = 'ERROR...ERROR';
-            } else {
-                displayValue = Math.round(solution * 1000) / 1000;
-                firstValue = displayValue;
-                solution = 0;
-            }
-            updateDisplay();
-        } 
-        else {
-            //second click = input of first operator
-            firstOperator = e.key;
-            firstValue = displayValue;
-        }
+        getOperators(e.key);
     }
     if (e.key === '=' || e.key === 'Enter') {
-        if (firstOperator === '') {
-            //pressing equals when there is no operator
-            displayValue = displayValue;
-        } else if (secondOperator != '') {
-            //if there is a second operator, next equals operation
-            secondValue = displayValue;
-            solution = operate(secondOperator, firstValue, secondValue);
-            if (solution === 'ERROR...ERROR') {
-                displayValue = 'ERROR...ERROR';
-            } else {
-                displayValue = Math.round(solution * 1000) / 1000;
-                firstValue = displayValue;
-                secondValue = 0;
-                firstOperator = '';
-                secondOperator = '';
-                solution = 0;
-            }
-            updateDisplay();
-        } else {
-            //first equals operaton
-            secondValue = displayValue;
-            solution = operate(firstOperator, firstValue, secondValue);
-            if (solution === 'ERROR...ERROR') {
-                displayValue = 'ERROR...ERROR';
-            } else {
-                displayValue = Math.round(solution * 1000) / 1000;
-                firstValue = displayValue;
-                secondValue = 0;
-                firstOperator = '';
-                secondOperator = '';
-                solution = 0;
-            }
-            updateDisplay();
-        }
+        getEquals();
     }
     if (e.key === 'Backspace') {
-        if (displayValue === 0) {
-            displayValue = displayValue;
-        } else {
-            displayValue = displayValue.toString().slice(0, -1);
-        }
+        deleteLastChar();
         updateDisplay();
     }
     if (e.key === 'Delete') {
+        clearEverything();
+        updateDisplay();
+    }
+});
+
+function getNumbers(number) {
+    if (firstValue === 0) {
+        // first click = input of first value
+        if (displayValue === 0) {
+            displayValue = number;
+        } else {
+            displayValue += number;
+        }
+    } else {
+        // third click = input of second value
+        if (displayValue === firstValue) {
+            displayValue = number;
+        } else {
+            displayValue += number;
+        }
+    }
+}
+
+function getOperators(operator) {
+    if(firstOperator != '' && secondOperator === '') {
+        //fourth click = if there is already the first operator,
+        //input of second operator
+        secondOperator = operator;
+        secondValue = displayValue;
+        solution = operate(firstOperator, firstValue, secondValue);
+        if (solution === 'ERROR...ERROR') {
+            displayValue = 'ERROR...ERROR';
+        } else {
+            displayValue = Math.round(solution * 1000) / 1000;
+            firstValue = displayValue;
+            solution = 0;
+        }
+        updateDisplay();
+    } else if (firstOperator != '' && secondOperator != '') {
+        //sixth click = input of new operator
+        secondValue = displayValue;
+        solution = operate(secondOperator, firstValue, secondValue);
+        secondOperator = operator;
+        if (solution === 'ERROR...ERROR') {
+            displayValue = 'ERROR...ERROR';
+        } else {
+            displayValue = Math.round(solution * 1000) / 1000;
+            firstValue = displayValue;
+            solution = 0;
+        }
+        updateDisplay();
+    } 
+    else {
+        //second click = input of first operator
+        firstOperator = operator;
+        firstValue = displayValue;
+    }    
+
+}
+
+function getEquals(){
+    if (firstOperator === '') {
+        //pressing equals when there is no operator
+        displayValue = displayValue;
+    } else if (secondOperator != '') {
+        //if there is a second operator, next equals operation
+        secondValue = displayValue;
+        solution = operate(secondOperator, firstValue, secondValue);
+        if (solution === 'ERROR...ERROR') {
+            displayValue = 'ERROR...ERROR';
+        } else {
+            displayValue = Math.round(solution * 1000) / 1000;
+            firstValue = displayValue;
+            secondValue = 0;
+            firstOperator = '';
+            secondOperator = '';
+            solution = 0;
+        }
+        updateDisplay();
+    } else {
+        //first equals operaton
+        secondValue = displayValue;
+        solution = operate(firstOperator, firstValue, secondValue);
+        if (solution === 'ERROR...ERROR') {
+            displayValue = 'ERROR...ERROR';
+        } else {
+            displayValue = Math.round(solution * 1000) / 1000;
+            firstValue = displayValue;
+            secondValue = 0;
+            firstOperator = '';
+            secondOperator = '';
+            solution = 0;
+        }
+        updateDisplay();
+    }
+}
+
+function clearEverything() {
         //reset of all variables
         displayValue = 0;
         firstValue = 0;
@@ -236,9 +161,15 @@ document.addEventListener('keydown', (e) => {
         firstOperator = '';
         secondOperator = '';
         solution = 0;
-        updateDisplay();
+}
+
+function deleteLastChar() {
+    if (displayValue === 0) {
+        displayValue = displayValue;
+    } else {
+        displayValue = displayValue.toString().slice(0, -1);
     }
-});
+}
 
 function add (a, b) {
     return parseInt(a) + parseInt(b);
